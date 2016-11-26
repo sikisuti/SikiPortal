04b766b249835ef6e7e60b8ea6af17d8aa942564
Siklosi <tamas.siklosi@accenture.com>
2016-11-26 22:29:33 +0100

    refuel and service datastorage into db implemented

--- apis/carInfoApi.js
+++ apis/carInfoApi.js
@@ -26,22 +26,70 @@ module.exports = function(app, pool){
     });
   });
 
-  app.post("/api/carInfo/cars", function(req, res){
-    console.log(req.body);
-    res.json({ message: "Created" });
-  });
-
-  app.post("/api/carInfo/refuelData", function(req, res){
-    var responseMessage;
+  app.get("/api/carInfo/refuelData", function(req, res){
     pool.getConnection(function(err, connection){
       if (err) { console.log(err); res.json({ message: err }); }
 
-      connection.query('INSERT INTO commonActionData values ?', { carId: req.body.carId, actionDate: req.body.actionDate, km: req.body.km }, function(err, commonResult){
-        if (err) { console.log(err); connection.release(); res.json({ message: err }) }
+      var query = connection.query(
+        'SELECT DATE_FORMAT(ca.actionDate, "%Y-%m-%d") AS actionDate , c.regNumber, r.fuelAmount ' +
+        'FROM cars c ' +
+        'INNER JOIN commonActionData ca ON ca.carId = c.id ' +
+        'INNER JOIN refuelData r ON r.commonId = ca.id', function(err, result){
+          if (err) { console.log(query.sql); console.log(err); connection.release(); res.json({ message: err }) }
 
-        connection.query('INSERT INTO refuelData values ?', { commonId: commonResult.insertId, fuelAmount: req.body.fuelAmount, fuelCost: req.body.fuelCost }, function(err, result){
-          if (err) { console.log(err); connection.release(); res.json({ message: err }) }
+          console.log(query.sql);
+          console.log(result);
+          connection.release();
+          res.json(result);
+        });
+    });
+  });
 
+  app.post("/api/carInfo/refuelData", function(req, res){
+    pool.getConnection(function(err, connection){
+      if (err) { console.log(err); res.json({ message: err }); }
+
+      var commonActionDataQuery = connection.query('INSERT INTO commonActionData set ?',
+          { carId: req.body.carId, actionDate: req.body.actionDate, km: req.body.km }, function(err, commonResult){
+        if (err) { console.log(commonActionDataQuery.sql); console.log(err); connection.release(); res.json({ message: err }) }
+
+        var refuelDataQuery = connection.query('INSERT INTO refuelData set ?', { commonId: commonResult.insertId, fuelAmount: req.body.fuelAmount, fuelCost: req.body.fuelCost }, function(err, result){
+          if (err) { console.log(commonActionDataQuery.sql); console.log(refuelDataQuery.sql); console.log(err); connection.release(); res.json({ message: err }) }
+
+          console.log(commonActionDataQuery.sql);
+          console.log(refuelDataQuery.sql);
+          connection.release();
+          res.json({ message: "Success" });
+        });
+      });
+    });
+  });
+
+  app.post("/api/carInfo/serviceData", function(req, res){
+    console.log(req.body);
+    pool.getConnection(function(err, connection){
+      if (err) { console.log(err); res.json({ message: err }); }
+
+      var commonActionDataQuery = connection.query('INSERT INTO commonActionData set ?',
+          { carId: req.body.carId, actionDate: req.body.actionDate, km: req.body.km }, function(err, commonResult){
+        if (err) { console.log(commonActionDataQuery.sql); console.log(err); connection.release(); res.json({ message: err }) }
+
+        var serviceDataQuery = connection.query('INSERT INTO serviceData set ?',
+            { commonId: commonResult.insertId, serviceCost: req.body.serviceCost }, function(err, serviceResult){
+          if (err) { console.log(commonActionDataQuery.sql); console.log(refuelDataQuery.sql); console.log(err); connection.release(); res.json({ message: err }) }
+
+          req.body.serviceItems.forEach(function(serviceItem){
+            var serviceItemQuery = connection.query('INSERT INTO serviceWorks set ?',
+                { serviceDataId: serviceResult.insertId, serviceItemId: serviceItem }, function(err, result){
+              if (err) { console.log(commonActionDataQuery.sql);
+                          console.log(refuelDataQuery.sql);
+                          console.log(serviceItemQuery.sql); console.log(err); connection.release(); res.json({ message: err }); }
+
+            });
+          });
+
+          console.log(commonActionDataQuery.sql);
+          console.log(serviceDataQuery.sql);
           connection.release();
           res.json({ message: "Success" });
         });
