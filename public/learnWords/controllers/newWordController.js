03200127025874bfc6342d3d3ad9d3de60460c2c
Siklosi <tamas.siklosi@accenture.com>
2017-01-28 23:15:27 +0100

    new word

--- public/learnWords/controllers/newWordController.js
+++ public/learnWords/controllers/newWordController.js
@@ -1,11 +1,23 @@
 learnWordsApp.controller('newWordController', ['$scope', '$location', '$http', function($scope, $location, $http){
+
+  var audio;
+
   $scope.newWord = {
-    foreign: "",
+    foreignWord: "",
     native: "",
-    definition: ""
+    definition: "",
+    exampleSentence: "",
+    lexicalCategory: "",
+    pronunciation: "",
+    audioFile: ""
   };
 
+  $scope.setNative = function(newNative) {
+        $scope.newWord.native = newNative;
+    };
+
   $scope.search = function(word) {
+    audio = undefined;
     $http.get('/learnWords/searchNatives?word=' + word)
       .then(function(response){
         $scope.natives = response.data;
@@ -16,9 +28,30 @@ learnWordsApp.controller('newWordController', ['$scope', '$location', '$http', f
     $http.get('/learnWords/searchOxford?word=' + word.replace(' ', '_'))
       .then(function(response){
         $scope.oxfords = response.data;
-        console.log(response.data);
       }, function(err){
         console.log(err);
       });
+  };
+
+  $scope.selectItem = function(item) {
+    $scope.newWord.definition = item.definition;
+    $scope.newWord.lexicalCategory = item.lexicalCategory;
+    $scope.newWord.exampleSentence = item.example;
+    $scope.newWord.pronunciation = item.pronunciation;
+    $scope.newWord.audioFile = item.audioFile;
+  };
+
+  $scope.submit = function() {
+    console.log($scope.newWord);
+  }
+
+  $scope.play = function(audioFile) {
+    if (audioFile != ""){
+      if (audio == undefined){
+        audio = new Howl({ src: [audioFile] });
+      }
+      audio.play();
+    }
+
   }
 }]);
