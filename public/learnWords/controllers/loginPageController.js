e6c1b428088ebc44a2bd34c031975c2f445e2e7b
Siklosi <tamas.siklosi@accenture.com>
2017-03-06 23:23:34 +0100

    fix busy

--- public/learnWords/controllers/loginPageController.js
+++ public/learnWords/controllers/loginPageController.js
@@ -1,4 +1,14 @@
 learnWordsApp.controller('loginPageController', ['$scope', '$location', '$http', '$cookies', function($scope, $location, $http, $cookies){
+
+  $scope.busy = {
+    state: false,
+    message: ""
+  };
+  var setBusy = function(state, message){
+    $scope.busy.state = state;
+    $scope.busy.message = state ? message : "";
+  };
+
     $scope.submit = function() {
       var data = {
         'username': $scope.username,
@@ -11,9 +21,12 @@ learnWordsApp.controller('loginPageController', ['$scope', '$location', '$http',
                 }
             }
 
+      setBusy(true, 'Logging in...');
       $http.post('/authorization/login', data, config).then(function(response){
+        setBusy(false);
         $location.path('/');
       }, function(response){
+        setBusy(false);
         console.log("Authentication failed");
       });
     };
--- public/learnWords/controllers/registerController.js
+++ public/learnWords/controllers/registerController.js
@@ -1,4 +1,14 @@
 learnWordsApp.controller('registerController', ['$scope', '$location', '$http', function($scope, $location, $http){
+
+  $scope.busy = {
+    state: false,
+    message: ""
+  };
+  var setBusy = function(state, message){
+    $scope.busy.state = state;
+    $scope.busy.message = state ? message : "";
+  };
+
     $scope.submit = function() {
       if ($scope.password != $scope.passwordConfirmation){
         alert('Password fields must be alike!');
@@ -15,10 +25,14 @@ learnWordsApp.controller('registerController', ['$scope', '$location', '$http', 
                 }
             }
 
+      setBusy(true, 'Registering...');
       $http.post('/userManagement/user', data, config).then(function(response){
-        if (response.status == 400) {alert('User already exists!'); return;}
+        if (response.status == 400) {setBusy(false); alert('User already exists!'); return;}
+
+        setBusy(false);
         $location.path('/login');
       }, function(response){
+        setBusy(false);
         alert(response.data);
         console.log(response);
       });
