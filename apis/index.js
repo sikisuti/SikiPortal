e80b6635a7437f6e8a529723bc7eba3dd6add9c8
Siklosi <tamas.siklosi@accenture.com>
2017-11-26 20:32:51 +0100

    remove mongo

--- apis/index.js
+++ apis/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 router.use('/learnJava', require('./learnJavaApi'));
 router.use('/learnWords', require('./learnWordsApi'));
-router.use('/shop', require('./shopApi'));
+//router.use('/shop', require('./shopApi'));
 
 module.exports = router;
