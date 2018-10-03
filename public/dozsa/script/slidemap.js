07b9405b3cfe136b37893f6037f0724b280b107b
Tamás Siklósi <sikisuti@gmail.com>
2018-10-03 20:48:48 +0200

    dozsa

---
+++ public/dozsa/script/slidemap.js
@@ -0,0 +1,19 @@
+let slidemap = [{
+    "title": "Nappali",
+    "folder": "living"
+}, {
+    "title": "Előszoba",
+    "folder": "hall"
+}, {
+    "title": "Fürdő",
+    "folder": "bath"
+}, {
+    "title": "WC",
+    "folder": "wc"
+}, {
+    "title": "Kisszoba",
+    "folder": "bedroom"
+}, {
+    "title": "Lépcsőház",
+    "folder": "staircase"
+}];
--- public/dozsa/script/slideshow.js
+++ public/dozsa/script/slideshow.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+var slideIndex = 0;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -12,17 +12,9 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  slideIndex = n % slidemap.length;
+  let slideContent = slidemap[slideIndex];
+  $('#title').html(slideContent.title);
+  let folder = slideContent.folder
+  $('#description').load('slide/' + folder + '/description.html');
 }
